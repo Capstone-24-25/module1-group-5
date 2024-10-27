71b209a971a4003b86d17bb5c2db5c8873c9bdb9
Jaxon Zhang <yangzhang@ucsb.edu>
2024-10-27 02:18:14 -0700

    [add] basics

--- scripts/preprocessing.R
+++ scripts/preprocessing.R
@@ -2,38 +2,56 @@ library(tidyverse)
 
 
 # get names
-var_names <- read_csv('data/biomarker-raw.csv', 
-                     col_names = F, 
-                     n_max = 2, 
-                     col_select = -(1:2)) %>%
+var_names <- read_csv("data/biomarker-raw.csv",
+  # prevent interpreting the first row as header
+  col_names = F,
+  # read only 2 rows
+  n_max = 2,
+  # exclude first two columns
+  col_select = -(1:2)
+) %>%
+  # switching rows and columns
   t() %>%
   as_tibble() %>%
-  rename(name = V1, 
-         abbreviation = V2) %>%
+  # name, abbreviate are the new name. V1, V2 are the the default old neames
+  rename(
+    name = V1,
+    abbreviation = V2
+  ) %>%
   na.omit()
 
 # function for trimming outliers (good idea??)
-trim <- function(x, .at){
-  x[abs(x) > .at] <- sign(x[abs(x) > .at])*.at
+trim <- function(x, .at) {
+  x[abs(x) > .at] <- sign(x[abs(x) > .at]) * .at
+  # sign() is used to preserve the sign of original value
   return(x)
 }
 
 # read in data
-biomarker_clean <- read_csv('data/biomarker-raw.csv', 
-         skip = 2,
-         col_select = -2L,
-         col_names = c('group', 
-                       'empty',
-                       pull(var_names, abbreviation),
-                       'ados'),
-         na = c('-', '')) %>%
+biomarker_clean <- read_csv("data/biomarker-raw.csv",
+  skip = 2,
+  col_select = -2L,
+  col_names = c(
+    "group",
+    "empty",
+    pull(var_names, abbreviation),
+    "ados"
+  ),
+  # Treat '-' and '' as NA.
+  na = c("-", "")
+) %>%
   filter(!is.na(group)) %>%
   # log transform, center and scale, and trim
-  mutate(across(.cols = -c(group, ados), 
-                ~ trim(scale(log10(.x))[, 1], .at = 3))) %>%
+  # accross apply the functions to all cols at once, but exclude group and ados here.
+  mutate(across(
+    .cols = -c(group, ados),
+    ~ trim(scale(log10(.x))[, 1], .at = 3)
+  )) %>%
   # reorder columns
   select(group, ados, everything())
 
 # export as r binary
-save(list = 'biomarker_clean', 
-     file = 'data/biomarker-clean.RData')
+save(
+  list = "biomarker_clean",
+  file = "data/biomarker-clean.RData"
+)
